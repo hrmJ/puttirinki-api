57c2f0d0fa04b3ff2cdbffb10bd19beee9587ab5
Juho Härme <juho.harme@gmail.com>
2021-06-30 16:50:46 +0300

    first deployment with serverless

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose';
 import { Application } from './declarations';
 import logger from './logger';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default function (app: Application): void {
   mongoose
-    .connect(app.get('mongodb'), {
+    .connect(process.env.MONGO_URI || app.get('mongouri'), {
       useCreateIndex: true,
       useNewUrlParser: true,
+      pass: process.env.MONGO_PASS || app.get('mongopass'),
+      user: process.env.MONGO_UNAME || app.get('mongouname'),
     })
     .catch((err) => {
       logger.error(err);
