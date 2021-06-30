9a76b47a3385985adc8abacd6aa16d0abd8b7f48
Juho Härme <juho.harme@gmail.com>
2021-06-30 19:11:22 +0300

    adjust mongo config based on node_env

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -5,17 +5,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default function (app: Application): void {
-  mongoose
-    .connect(process.env.MONGO_URI || app.get('mongouri'), {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      pass: process.env.MONGO_PASS || app.get('mongopass'),
-      user: process.env.MONGO_UNAME || app.get('mongouname'),
-    })
-    .catch((err) => {
-      logger.error(err);
-      process.exit(1);
-    });
+  const basicOpts = { useCreateIndex: true, useNewUrlParser: true };
+  const connectionOpts =
+    process.env.NODE_ENV === 'production'
+      ? {
+          ...basicOpts,
+          pass: process.env.MONGO_PASS || app.get('mongopass'),
+          user: process.env.MONGO_UNAME || app.get('mongouname'),
+        }
+      : { ...basicOpts };
+  mongoose.connect(process.env.MONGO_URI || app.get('mongouri'), connectionOpts).catch((err) => {
+    logger.error(err);
+    process.exit(1);
+  });
 
   app.set('mongooseClient', mongoose);
 }
