c2dc9c21148ce65639b0b23462550ee052c291f5
Juho Härme <juho.harme@gmail.com>
2021-07-01 09:12:52 +0300

    fix missing proxy statement in serverless.yml
    and try to use aws iam for atlas auth in prod

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -5,15 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default function (app: Application): void {
-  const basicOpts = { useCreateIndex: true, useNewUrlParser: true };
-  const connectionOpts =
-    process.env.NODE_ENV === 'production'
-      ? {
-          ...basicOpts,
-          pass: process.env.MONGO_PASS || app.get('mongopass'),
-          user: process.env.MONGO_UNAME || app.get('mongouname'),
-        }
-      : { ...basicOpts };
+  const connectionOpts = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true };
   mongoose.connect(process.env.MONGO_URI || app.get('mongouri'), connectionOpts).catch((err) => {
     logger.error(err);
     process.exit(1);
--- src/prod.ts
+++ src/prod.ts
@@ -1,4 +1,12 @@
 import app from './app';
 import serverless from 'serverless-http';
 
-export default { handler: serverless(app) };
+const handler = serverless(app);
+module.exports.handler = async (event: any, context: any) => {
+  // you can do other things here
+  const result = await handler(event, context);
+  // and here
+  return result;
+};
+
+//export default { handler: serverless(app) };
